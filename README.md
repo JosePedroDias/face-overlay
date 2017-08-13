528479dd4b5ee0bf7301a29e64ad1cc321a8fe03
José Pedro Dias <jose.pedro.dias@gmail.com>
2017-08-13 12:06:39 +0100

    v1

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # face overlay
 
+First version is on JSBin, the [bas dost song from Rihanna](https://output.jsbin.com/tosotuy/latest/quiet).
+
+## how to adapt
+
+1. Choose a video from github. Write down it's id, width and height (`YTID`, `W`, `H`). Also choose the start and end times for your loop animation (`TS`, `TE`).
+2. Look for pictures of the replacement face. images.google.com are very effective. Keep in mind the pose you're looking for.
+3. Edit the replacement face in Photoshop/GIMP/etc - delete the surroundings. Save as PNG and write down width and height (scale down if too large first). `face.png`, ´
+4. Edit `index.html` to make it match the viewport width of the video and confirm the `face.png` exists.
+5. Edit the costants above in `main.js`
+6. Reset `timeline.js` to an empty array. Uncomment `p.setPlaybackRate(0.25);` line in `main.js`.
+7. Loop endlessly through the animation cricking in the center of the face when and where it should appear. Export the data with `console.log(JSON.stringify(o))` and place it in `timeline.js`.
+8. Remove faulty keyframes (when you clicked for too long), elect keys where the face changes size, if any (each item in an array with width, height and optionally a class to be assigned).
+9. Comment `p.setPlaybackRate(0.25);` line in `main.js`.
+10. Publish :D
+
+## reference
+
+<https://developers.google.com/youtube/iframe_api_reference>
---
+++ main.js
@@ -0,0 +1,95 @@
+"use strict";
+
+var YTID = "fyaI4-5849w";
+var W = 854 / 2;
+var H = 480 / 2;
+var TS = 30;
+var TE = 50;
+
+var p;
+function onYouTubeIframeAPIReady() {
+  p = new YT.Player("yt_target", {
+    width: W,
+    height: H,
+    videoId: YTID,
+    playerVars: {
+      controls: 0,
+      showinfo: 0,
+      rel: 0,
+      fs: 0
+    },
+    events: {
+      onReady: onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(ev) {
+  p.playVideo();
+  //console.log( p.getAvailablePlaybackRates() );
+  //p.setPlaybackRate(0.25);
+  p.seekTo(TS);
+  var i = document.querySelector("img");
+  setInterval(function() {
+    var t0 = p.getCurrentTime();
+    t = (~~(t0 * 4) / 4).toFixed(2);
+    if (t0 > TE) {
+      p.seekTo(30);
+    }
+    var pos;
+    if (down) {
+      pos = [x, y];
+      o[t] = pos;
+    } else {
+      pos = o[t];
+    }
+
+    var pos = o[t];
+    if (pos) {
+      i.style.left = pos[0] + "px";
+      i.style.top = pos[1] + "px";
+      if (pos[2]) {
+        i.className = "i" + pos[2];
+      }
+      i.style.opacity = 1;
+      //console.log(t);
+    } else {
+      i.style.opacity = 0;
+      //console.log('---');
+    }
+  }, 220);
+}
+
+var t, x, y;
+
+var down = false;
+var over = document.querySelector("#over");
+over.style.width = W + "px";
+over.style.height = H + "px";
+
+function stop(ev) {
+  ev.preventDefault();
+  ev.stopPropagation();
+}
+
+var b = document.querySelector("button");
+b.addEventListener("click", function() {
+  p.playVideo();
+});
+
+over.addEventListener("mousedown", function() {
+  down = true;
+});
+
+over.addEventListener("mouseup", function(ev) {
+  down = false;
+  stop(ev);
+});
+
+over.addEventListener("mousemove", function(ev) {
+  stop(ev);
+  if (down) {
+    x = ev.clientX;
+    y = ev.clientY;
+  }
+});
